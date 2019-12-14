3ee841f867fb3747aa5289806a2b3e3bde959a5d
amrutraomohite <amrutraomohite@gmail.com>
2019-12-14 22:07:04 +0530

    code changes parking lot

--- ParkingLot_v2.0.0/src/main/java/parkinglot/Commands.java
+++ ParkingLot_v2.0.0/src/main/java/parkinglot/Commands.java
@@ -3,14 +3,34 @@ package com.parkinglot;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 import com.gojek.parkinglot.InputParser;
+import com.gojek.parkinglot.ParkingLot;
 import com.gojek.parkinglot.ParkingLot.Car;
 
 public class Commands {
+	  public Map<String, Method> commandsMap;
 
+	    public Commands() {
+	        commandsMap = new HashMap<String, Method>();
+	        try {
+	            populateCommandsHashMap();
+	        } catch (NoSuchMethodException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    private void populateCommandsHashMap() throws NoSuchMethodException {
+	        commandsMap.put("create_parking_lot", ParkingLot.class.getMethod("createParkingLot", String.class));
+	        commandsMap.put("park", ParkingLot.class.getMethod("park", String.class, String.class));
+	        commandsMap.put("leave", ParkingLot.class.getMethod("leave", String.class));
+	        commandsMap.put("status", ParkingLot.class.getMethod("status"));
+	        commandsMap.put("registration_numbers_for_cars_with_colour", ParkingLot.class.getMethod("getRegistrationNumbersFromColor", String.class));
+	        commandsMap.put("slot_numbers_for_cars_with_colour", ParkingLot.class.getMethod("getSlotNumbersFromColor", String.class));
+	        commandsMap.put("slot_number_for_registration_number", ParkingLot.class.getMethod("getSlotNumberFromRegNo", String.class));
+	    }
 }
