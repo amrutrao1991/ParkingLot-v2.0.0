9075d85cf947e74c266f8ca88b1889d6b4171d40
amrutraomohite <amrutraomohite@gmail.com>
2019-12-14 22:04:50 +0530

    Added new class.

---
+++ ParkingLot_v2.0.0/src/main/java/parkinglot/Commands.java
@@ -0,0 +1,16 @@
+package com.parkinglot;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.gojek.parkinglot.InputParser;
+import com.gojek.parkinglot.ParkingLot.Car;
+
+public class Commands {
+
+}
